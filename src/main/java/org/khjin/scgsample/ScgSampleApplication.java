ac7f6b3858163393320c1acf82a114e71128bb1f
Horilla <khjin1197@gmail.com>
2024-05-08 00:17:04 +0900

    initial commit

---
+++ src/main/java/org/khjin/scgsample/ScgSampleApplication.java
@@ -0,0 +1,13 @@
+package org.khjin.scgsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khjin/scgsample/ScgSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khjin.scgsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
